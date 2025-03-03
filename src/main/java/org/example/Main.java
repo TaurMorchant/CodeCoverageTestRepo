d98b519fa1ff76fc959f45043cde880df67338eb
larkin <larkin.qubership@gmail.com>
2025-03-03 17:09:45 +0400

    First commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(plus(1, 2));
+    }
+
+    public static int plus(int a, int b) {
+        return a + b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+}
---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @org.junit.jupiter.api.Test
+    void plus() {
+        assertEquals(5, Main.plus(2, 3));
+    }
+}
