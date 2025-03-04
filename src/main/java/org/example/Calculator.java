839c3206366f8a7d185ce17f956dde49428a34fa
larkin <larkin.qubership@gmail.com>
2025-03-04 13:45:15 +0400

    Add minus operation

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -8,4 +8,11 @@ public final class Calculator {
     public static int multiply(int a, int b) {
         return a * b;
     }
+
+    public static int minus(int a, int b) {
+        if (a < b) {
+            throw new IllegalArgumentException("Oh, no, negative numbers!");
+        }
+        return a - b;
+    }
 }
